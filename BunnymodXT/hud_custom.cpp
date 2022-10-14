805c848aa3431292255945847f5371f4700a23d1
khang <mkhangle20@gmail.com>
2022-10-14 01:56:16 -0400

    Change things around

--- BunnymodXT/hud_custom.cpp
+++ BunnymodXT/hud_custom.cpp
@@ -77,9 +77,7 @@ namespace CustomHud
 
 		float vel;
 		float zvel;
-		float xpos;
-		float ypos;
-		float zpos;
+		float pos[3];
 		float realyaw;
 		float health;
 		float armor;
@@ -546,18 +544,9 @@ namespace CustomHud
 			}
 			else
 			{
-				if (frame_bulk_selected)
-				{
-					out << "X: " << frame_bulk_status.xpos << "\n"
-						<< "Y: " << frame_bulk_status.ypos << "\n"
-						<< "Z: " << frame_bulk_status.zpos;
-				}
-				else
-				{
-					out << "X: " << player.origin[0] << "\n"
-						<< "Y: " << player.origin[1] << "\n"
-						<< "Z: " << player.origin[2];
-				}
+				out << "X: " << player.origin[0] << "\n"
+					<< "Y: " << player.origin[1] << "\n"
+					<< "Z: " << player.origin[2];
 			}
 
 			DrawMultilineString(x, y, out.str());
@@ -1333,6 +1322,10 @@ namespace CustomHud
 			out << "Vel: " << frame_bulk_status.vel << '\n';
 			out << "Z Vel: " << frame_bulk_status.zvel << '\n';
 
+			out << "X Pos: " << frame_bulk_status.pos[0] << "\n"
+				<< "Y Pos: " << frame_bulk_status.pos[1] << "\n"
+				<< "Z Pos: " << frame_bulk_status.pos[2];
+
 			out << "Health: " << frame_bulk_status.health << '\n';
 			out << "Armor: " << frame_bulk_status.armor << '\n';
 		} else {
@@ -1756,16 +1749,16 @@ namespace CustomHud
 		return si;
 	}
 
-	void UpdateTASEditorStatus(const HLTAS::Frame& frame_bulk, const float& player_vel, const float& player_zvel, const float& player_xpos, const float& player_ypos, const float& player_zpos, const float& player_realyaw, const float& player_health, const float& player_armor, const float& player_stamina)
+	void UpdateTASEditorStatus(const HLTAS::Frame& frame_bulk, const float& player_vel, const float& player_zvel, const float player_pos[3], const float& player_realyaw, const float& player_health, const float& player_armor, const float& player_stamina)
 	{
 		frame_bulk_selected = true;
 		frame_bulk_status = FrameBulkStatus{};
 
 		frame_bulk_status.vel = player_vel;
 		frame_bulk_status.zvel = player_zvel;
-		frame_bulk_status.xpos = player_xpos;
-		frame_bulk_status.ypos = player_ypos;
-		frame_bulk_status.zpos = player_zpos;
+		frame_bulk_status.pos[0] = player_pos[0];
+		frame_bulk_status.pos[1] = player_pos[1];
+		frame_bulk_status.pos[2] = player_pos[2];
 		frame_bulk_status.realyaw = player_realyaw;
 
 		frame_bulk_status.strafe = frame_bulk.Strafe;

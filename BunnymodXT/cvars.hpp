025236ee6fcee032bee9ea712a0ec646645b9218
SmileyAG <redstyle45@gmail.com>
2022-12-02 23:01:01 +0400

    Rework of cvar defining

--- BunnymodXT/cvars.hpp
+++ BunnymodXT/cvars.hpp
@@ -1,5 +1,195 @@
 #pragma once
 
+#define FIND_CVARS(X) \
+	X(con_color) \
+	X(sv_cheats) \
+	X(sv_maxvelocity) \
+	X(sv_maxspeed) \
+	X(sv_stopspeed) \
+	X(sv_friction) \
+	X(edgefriction) \
+	X(sv_accelerate) \
+	X(sv_airaccelerate) \
+	X(sv_gravity) \
+	X(sv_stepsize) \
+	X(sv_bounce) \
+	X(fps_max) \
+	X(default_fov) \
+	X(skill)
+
+#define DEFINE_CVARS(X) \
+	X(_bxt_taslog, "0") \
+	X(_bxt_min_frametime, "0") \
+	X(_bxt_norefresh, "0") \
+	X(_bxt_bunnysplit_time_update_frequency, "41") \
+	X(_bxt_save_runtime_data_in_demos, "1") \
+	X(_bxt_tas_script_generation, "1337") \
+	X(bxt_taslog_filename, "taslogger.log") \
+	X(bxt_autopause, "0") \
+	X(bxt_interprocess_enable, "0") \
+	X(bxt_shake_remove, "0") \
+	X(bxt_fade_remove, "0") \
+	X(bxt_skybox_remove, "0") \
+	X(bxt_water_remove, "0") \
+	X(bxt_stop_demo_on_changelevel, "0") \
+	X(bxt_tas_editor_simulate_for_ms, "40") \
+	X(bxt_tas_editor_camera_editor, "0") \
+	X(bxt_tas_editor_apply_smoothing_over_s, "0.15") \
+	X(_bxt_tas_editor_apply_smoothing_high_weight_duration, "0.03") \
+	X(_bxt_tas_editor_apply_smoothing_high_weight_multiplier, "3") \
+	X(bxt_tas_norefresh_until_last_frames, "0") \
+	X(bxt_tas_write_log, "0") \
+	X(bxt_tas_playback_speed, "1") \
+	X(bxt_disable_vgui, "0") \
+	X(bxt_show_only_viewmodel_and_player, "0") \
+	X(bxt_force_zmax, "0") \
+	X(bxt_viewmodel_fov, "0") \
+	X(bxt_viewmodel_disable_idle, "0") \
+	X(bxt_viewmodel_disable_equip, "0") \
+	X(bxt_viewmodel_semitransparent, "0") \
+	X(bxt_clear_color, "") \
+	X(bxt_force_fov, "0") \
+	X(bxt_force_clear, "0") \
+	X(bxt_fix_mouse_horizontal_limit, "0") \
+	X(bxt_hud_game_color, "") \
+	X(bxt_disable_gamedir_check_in_demo, "0") \
+	X(bxt_remove_fps_limit, "0") \
+	X(bxt_cof_disable_save_lock, "0") \
+	X(bxt_fire_on_stuck, "") \
+	X(bxt_autojump, "0") \
+	X(bxt_autojump_priority, "1") \
+	X(bxt_bhopcap, "1") \
+	X(bxt_timer_autostop, "1") \
+	X(bxt_show_hidden_entities, "0") \
+	X(bxt_show_hidden_entities_clientside, "0") \
+	X(bxt_show_triggers_legacy, "0") \
+	X(bxt_show_pickup_bbox, "0") \
+	X(bxt_disable_autosave, "0") \
+	X(bxt_disable_changelevel, "0") \
+	X(bxt_force_duck, "0") \
+	X(bxt_force_jumpless, "0") \
+	X(bxt_render_far_entities, "0") \
+	X(bxt_fire_on_mm_targetname, "") \
+	X(bxt_fire_on_mm_command, "") \
+	X(bxt_fire_on_button_target, "") \
+	X(bxt_fire_on_button_command, "") \
+	X(bxt_remove_stamina, "0") \
+	X(bxt_cof_enable_ducktap, "0") \
+	X(bxt_cof_slowdown_if_use_on_ground, "0") \
+	X(cl_righthand, "0") \
+	X(bxt_remove_punchangles, "0") \
+	X(bxt_hide_other_players, "0") \
+	X(bxt_disable_hud, "0") \
+	X(bxt_disable_nightvision_sprite, "0") \
+	X(bxt_autojump_prediction, "0") \
+	X(bxt_bhopcap_prediction, "1") \
+	X(bxt_show_nodes, "0") \
+	X(bxt_show_displacer_earth_targets, "0") \
+	X(bxt_show_custom_triggers, "1") \
+	X(bxt_triggers_color, "") \
+	X(bxt_wallhack, "0") \
+	X(bxt_wallhack_additive, "0") \
+	X(bxt_wallhack_alpha, "0.6") \
+	X(bxt_novis, "0") \
+	X(bxt_show_triggers, "0") \
+	X(bxt_collision_depth_map, "0") \
+	X(bxt_collision_depth_map_colors, "0") \
+	X(bxt_collision_depth_map_hull, "2") \
+	X(bxt_collision_depth_map_max_depth, "1000") \
+	X(bxt_collision_depth_map_pixel_scale, "8") \
+	X(bxt_collision_depth_map_remove_distance_limit, "1") \
+	X(bxt_unlock_camera_during_pause, "0") \
+	X(bxt_hud, "1") \
+	X(bxt_hud_color, "") \
+	X(bxt_hud_game_alpha_max_clientside, "0") \
+	X(bxt_hud_precision, "6") \
+	X(bxt_hud_quickgauss, "0") \
+	X(bxt_hud_quickgauss_offset, "") \
+	X(bxt_hud_quickgauss_anchor, "0.5 0") \
+	X(bxt_hud_velocity, "0") \
+	X(bxt_hud_velocity_offset, "") \
+	X(bxt_hud_velocity_anchor, "1 0") \
+	X(bxt_hud_origin, "0") \
+	X(bxt_hud_origin_offset, "") \
+	X(bxt_hud_origin_anchor, "1 0") \
+	X(bxt_hud_viewangles, "0") \
+	X(bxt_hud_viewangles_offset, "") \
+	X(bxt_hud_viewangles_anchor, "1 0") \
+	X(bxt_hud_distance, "0") \
+	X(bxt_hud_distance_offset, "") \
+	X(bxt_hud_distance_anchor, "1 0") \
+	X(bxt_hud_entity_info, "0") \
+	X(bxt_hud_entity_info_offset, "") \
+	X(bxt_hud_entity_info_anchor, "1 0") \
+	X(bxt_hud_selfgauss, "0") \
+	X(bxt_hud_selfgauss_offset, "") \
+	X(bxt_hud_selfgauss_anchor, "1 0") \
+	X(bxt_hud_armor, "0") \
+	X(bxt_hud_armor_offset, "") \
+	X(bxt_hud_armor_anchor, "1 0") \
+	X(bxt_hud_waterlevel, "0") \
+	X(bxt_hud_waterlevel_offset, "") \
+	X(bxt_hud_waterlevel_anchor, "1 0") \
+	X(bxt_hud_speedometer, "1") \
+	X(bxt_hud_speedometer_offset, "") \
+	X(bxt_hud_speedometer_anchor, "0.5 1") \
+	X(bxt_hud_jumpspeed, "0") \
+	X(bxt_hud_jumpspeed_offset, "") \
+	X(bxt_hud_jumpspeed_anchor, "0.5 1") \
+	X(bxt_hud_jumpdistance, "0") \
+	X(bxt_hud_jumpdistance_offset, "") \
+	X(bxt_hud_jumpdistance_anchor, "0.5 1") \
+	X(bxt_hud_health, "0") \
+	X(bxt_hud_health_offset, "") \
+	X(bxt_hud_health_anchor, "0.5 1") \
+	X(bxt_hud_health_override_in_demo, "0") \
+	X(bxt_hud_nihilanth, "0") \
+	X(bxt_hud_nihilanth_offset, "") \
+	X(bxt_hud_nihilanth_anchor, "1 0") \
+	X(bxt_hud_gonarch, "0") \
+	X(bxt_hud_gonarch_offset, "") \
+	X(bxt_hud_gonarch_anchor, "1 0") \
+	X(bxt_hud_timer, "0") \
+	X(bxt_hud_timer_offset, "") \
+	X(bxt_hud_timer_anchor, "0 0.5") \
+	X(bxt_hud_visible_landmarks, "0") \
+	X(bxt_hud_visible_landmarks_offset, "") \
+	X(bxt_hud_visible_landmarks_anchor, "0.5 0") \
+	X(bxt_hud_incorrect_fps_indicator, "1") \
+	X(bxt_hud_useables, "0") \
+	X(bxt_hud_useables_radius, "64") \
+	X(bxt_hud_tas_editor_status, "0") \
+	X(bxt_hud_tas_editor_status_offset, "") \
+	X(bxt_hud_tas_editor_status_anchor, "0 0") \
+	X(bxt_hud_entities, "0") \
+	X(bxt_hud_entities_offset, "") \
+	X(bxt_hud_entities_anchor, "0 0") \
+	X(bxt_hud_stamina, "0") \
+	X(bxt_hud_stamina_offset, "") \
+	X(bxt_hud_stamina_anchor, "0.5 0") \
+	X(bxt_cross, "0") \
+	X(bxt_cross_color, "") \
+	X(bxt_cross_alpha, "") \
+	X(bxt_cross_thickness, "2") \
+	X(bxt_cross_size, "10") \
+	X(bxt_cross_gap, "3") \
+	X(bxt_cross_outline, "0") \
+	X(bxt_cross_circle_radius, "0") \
+	X(bxt_cross_dot_color, "") \
+	X(bxt_cross_dot_size, "0") \
+	X(bxt_cross_top_line, "1") \
+	X(bxt_cross_bottom_line, "1") \
+	X(bxt_cross_left_line, "1") \
+	X(bxt_cross_right_line, "1") \
+	X(bxt_viewmodel_ofs_forward, "0") \
+	X(bxt_viewmodel_ofs_right, "0") \
+	X(bxt_viewmodel_ofs_up, "0") \
+	X(bxt_viewmodel_bob_angled, "0") \
+	X(bxt_show_bullets, "0") \
+	X(bxt_show_bullets_enemy, "0") \
+	X(bxt_anglespeed_cap, "1") \
+	X(bxt_speed_scaling, "1")
+
 #define FIND_WRAPPER(name) CVarWrapper name;
 #define DEFINE_WRAPPER(name, string) CVarWrapper name(#name, string);
 #define ADD_TO_FIND_LIST(name) &name,
@@ -124,198 +314,8 @@ inline void CVarWrapper::Set(const char* string)
 
 namespace CVars
 {
-	// Engine CVars
-	extern CVarWrapper _bxt_taslog;
-	extern CVarWrapper _bxt_min_frametime;
-	extern CVarWrapper _bxt_norefresh;
-	extern CVarWrapper _bxt_bunnysplit_time_update_frequency;
-	extern CVarWrapper _bxt_save_runtime_data_in_demos;
-	extern CVarWrapper _bxt_tas_script_generation;
-	extern CVarWrapper bxt_taslog_filename;
-	extern CVarWrapper bxt_autopause;
-	extern CVarWrapper bxt_interprocess_enable;
-	extern CVarWrapper bxt_fade_remove;
-	extern CVarWrapper bxt_shake_remove;
-	extern CVarWrapper bxt_skybox_remove;
-	extern CVarWrapper bxt_water_remove;
-	extern CVarWrapper bxt_stop_demo_on_changelevel;
-	extern CVarWrapper bxt_tas_editor_simulate_for_ms;
-	extern CVarWrapper bxt_tas_editor_camera_editor;
-	extern CVarWrapper bxt_tas_editor_apply_smoothing_over_s;
-	extern CVarWrapper _bxt_tas_editor_apply_smoothing_high_weight_duration;
-	extern CVarWrapper _bxt_tas_editor_apply_smoothing_high_weight_multiplier;
-	extern CVarWrapper bxt_tas_norefresh_until_last_frames;
-	extern CVarWrapper bxt_tas_write_log;
-	extern CVarWrapper bxt_tas_playback_speed;
-	extern CVarWrapper bxt_disable_vgui;
-	extern CVarWrapper bxt_show_only_viewmodel_and_player;
-	extern CVarWrapper bxt_force_zmax;
-	extern CVarWrapper bxt_viewmodel_fov;
-	extern CVarWrapper bxt_viewmodel_disable_idle;
-	extern CVarWrapper bxt_viewmodel_disable_equip;
-	extern CVarWrapper bxt_viewmodel_semitransparent;
-	extern CVarWrapper bxt_clear_color;
-	extern CVarWrapper bxt_force_fov;
-	extern CVarWrapper bxt_force_clear;
-	extern CVarWrapper bxt_fix_mouse_horizontal_limit;
-	extern CVarWrapper bxt_hud_game_color;
-	extern CVarWrapper bxt_disable_gamedir_check_in_demo;
-	extern CVarWrapper bxt_remove_fps_limit;
-	extern CVarWrapper bxt_cof_disable_save_lock;
-
-	extern CVarWrapper con_color;
-	extern CVarWrapper sv_cheats;
-	extern CVarWrapper sv_maxvelocity;
-	extern CVarWrapper sv_maxspeed;
-	extern CVarWrapper sv_stopspeed;
-	extern CVarWrapper sv_friction;
-	extern CVarWrapper edgefriction;
-	extern CVarWrapper sv_accelerate;
-	extern CVarWrapper sv_airaccelerate;
-	extern CVarWrapper sv_gravity;
-	extern CVarWrapper sv_stepsize;
-	extern CVarWrapper sv_bounce;
-	extern CVarWrapper fps_max;
-	extern CVarWrapper default_fov;
-	extern CVarWrapper skill;
-
-	// Serverside CVars
-	extern CVarWrapper bxt_fire_on_stuck;
-	extern CVarWrapper bxt_autojump;
-	extern CVarWrapper bxt_autojump_priority;
-	extern CVarWrapper bxt_bhopcap;
-	extern CVarWrapper bxt_timer_autostop;
-	extern CVarWrapper bxt_show_hidden_entities;
-	extern CVarWrapper bxt_show_hidden_entities_clientside;
-	extern CVarWrapper bxt_show_triggers_legacy;
-	extern CVarWrapper bxt_show_pickup_bbox;
-	extern CVarWrapper bxt_disable_autosave;
-	extern CVarWrapper bxt_disable_changelevel;
-	extern CVarWrapper bxt_force_duck;
-	extern CVarWrapper bxt_force_jumpless;
-	extern CVarWrapper bxt_render_far_entities;
-	extern CVarWrapper bxt_fire_on_mm_targetname;
-	extern CVarWrapper bxt_fire_on_mm_command;
-	extern CVarWrapper bxt_fire_on_button_target;
-	extern CVarWrapper bxt_fire_on_button_command;
-	extern CVarWrapper bxt_remove_stamina;
-	extern CVarWrapper bxt_cof_enable_ducktap;
-	extern CVarWrapper bxt_cof_slowdown_if_use_on_ground;
-
-	// Clientside CVars
-	extern CVarWrapper cl_righthand;
-	extern CVarWrapper bxt_remove_punchangles;
-	extern CVarWrapper bxt_hide_other_players;
-	extern CVarWrapper bxt_anglespeed_cap;
-	extern CVarWrapper bxt_speed_scaling;
-	extern CVarWrapper bxt_disable_hud;
-	extern CVarWrapper bxt_disable_nightvision_sprite;
-	extern CVarWrapper bxt_autojump_prediction;
-	extern CVarWrapper bxt_bhopcap_prediction;
-	extern CVarWrapper bxt_show_nodes;
-	extern CVarWrapper bxt_show_displacer_earth_targets;
-	extern CVarWrapper bxt_show_custom_triggers;
-	extern CVarWrapper bxt_triggers_color;
-	extern CVarWrapper bxt_wallhack;
-	extern CVarWrapper bxt_wallhack_additive;
-	extern CVarWrapper bxt_wallhack_alpha;
-	extern CVarWrapper bxt_novis;
-	extern CVarWrapper bxt_show_triggers;
-	extern CVarWrapper bxt_collision_depth_map;
-	extern CVarWrapper bxt_collision_depth_map_colors;
-	extern CVarWrapper bxt_collision_depth_map_hull;
-	extern CVarWrapper bxt_collision_depth_map_max_depth;
-	extern CVarWrapper bxt_collision_depth_map_pixel_scale;
-	extern CVarWrapper bxt_collision_depth_map_remove_distance_limit;
-	extern CVarWrapper bxt_unlock_camera_during_pause;
-	extern CVarWrapper bxt_hud;
-	extern CVarWrapper bxt_hud_color;
-	extern CVarWrapper bxt_hud_game_alpha_max_clientside;
-	extern CVarWrapper bxt_hud_precision;
-	extern CVarWrapper bxt_hud_quickgauss;
-	extern CVarWrapper bxt_hud_quickgauss_offset;
-	extern CVarWrapper bxt_hud_quickgauss_anchor;
-	extern CVarWrapper bxt_hud_velocity;
-	extern CVarWrapper bxt_hud_velocity_offset;
-	extern CVarWrapper bxt_hud_velocity_anchor;
-	extern CVarWrapper bxt_hud_origin;
-	extern CVarWrapper bxt_hud_origin_offset;
-	extern CVarWrapper bxt_hud_origin_anchor;
-	extern CVarWrapper bxt_hud_viewangles;
-	extern CVarWrapper bxt_hud_viewangles_offset;
-	extern CVarWrapper bxt_hud_viewangles_anchor;
-	extern CVarWrapper bxt_hud_distance;
-	extern CVarWrapper bxt_hud_distance_offset;
-	extern CVarWrapper bxt_hud_distance_anchor;
-	extern CVarWrapper bxt_hud_entity_info;
-	extern CVarWrapper bxt_hud_entity_info_offset;
-	extern CVarWrapper bxt_hud_entity_info_anchor;
-	extern CVarWrapper bxt_hud_selfgauss;
-	extern CVarWrapper bxt_hud_selfgauss_offset;
-	extern CVarWrapper bxt_hud_selfgauss_anchor;
-	extern CVarWrapper bxt_hud_armor;
-	extern CVarWrapper bxt_hud_armor_offset;
-	extern CVarWrapper bxt_hud_armor_anchor;
-	extern CVarWrapper bxt_hud_waterlevel;
-	extern CVarWrapper bxt_hud_waterlevel_offset;
-	extern CVarWrapper bxt_hud_waterlevel_anchor;
-	extern CVarWrapper bxt_hud_speedometer;
-	extern CVarWrapper bxt_hud_speedometer_offset;
-	extern CVarWrapper bxt_hud_speedometer_anchor;
-	extern CVarWrapper bxt_hud_jumpspeed;
-	extern CVarWrapper bxt_hud_jumpspeed_offset;
-	extern CVarWrapper bxt_hud_jumpspeed_anchor;
-	extern CVarWrapper bxt_hud_jumpdistance;
-	extern CVarWrapper bxt_hud_jumpdistance_offset;
-	extern CVarWrapper bxt_hud_jumpdistance_anchor;
-	extern CVarWrapper bxt_hud_health;
-	extern CVarWrapper bxt_hud_health_offset;
-	extern CVarWrapper bxt_hud_health_anchor;
-	extern CVarWrapper bxt_hud_health_override_in_demo;
-	extern CVarWrapper bxt_hud_nihilanth;
-	extern CVarWrapper bxt_hud_nihilanth_offset;
-	extern CVarWrapper bxt_hud_nihilanth_anchor;
-	extern CVarWrapper bxt_hud_gonarch;
-	extern CVarWrapper bxt_hud_gonarch_offset;
-	extern CVarWrapper bxt_hud_gonarch_anchor;
-	extern CVarWrapper bxt_hud_timer;
-	extern CVarWrapper bxt_hud_timer_offset;
-	extern CVarWrapper bxt_hud_timer_anchor;
-	extern CVarWrapper bxt_hud_visible_landmarks;
-	extern CVarWrapper bxt_hud_visible_landmarks_offset;
-	extern CVarWrapper bxt_hud_visible_landmarks_anchor;
-	extern CVarWrapper bxt_hud_incorrect_fps_indicator;
-	extern CVarWrapper bxt_hud_useables;
-	extern CVarWrapper bxt_hud_useables_radius;
-	extern CVarWrapper bxt_hud_tas_editor_status;
-	extern CVarWrapper bxt_hud_tas_editor_status_offset;
-	extern CVarWrapper bxt_hud_tas_editor_status_anchor;
-	extern CVarWrapper bxt_hud_entities;
-	extern CVarWrapper bxt_hud_entities_offset;
-	extern CVarWrapper bxt_hud_entities_anchor;
-	extern CVarWrapper bxt_hud_stamina;
-	extern CVarWrapper bxt_hud_stamina_offset;
-	extern CVarWrapper bxt_hud_stamina_anchor;
-	extern CVarWrapper bxt_cross;
-	extern CVarWrapper bxt_cross_color;
-	extern CVarWrapper bxt_cross_alpha;
-	extern CVarWrapper bxt_cross_thickness;
-	extern CVarWrapper bxt_cross_size;
-	extern CVarWrapper bxt_cross_gap;
-	extern CVarWrapper bxt_cross_outline;
-	extern CVarWrapper bxt_cross_circle_radius;
-	extern CVarWrapper bxt_cross_dot_color;
-	extern CVarWrapper bxt_cross_dot_size;
-	extern CVarWrapper bxt_cross_top_line;
-	extern CVarWrapper bxt_cross_bottom_line;
-	extern CVarWrapper bxt_cross_left_line;
-	extern CVarWrapper bxt_cross_right_line;
-	extern CVarWrapper bxt_viewmodel_ofs_forward;
-	extern CVarWrapper bxt_viewmodel_ofs_right;
-	extern CVarWrapper bxt_viewmodel_ofs_up;
-	extern CVarWrapper bxt_viewmodel_bob_angled;
-	extern CVarWrapper bxt_show_bullets;
-	extern CVarWrapper bxt_show_bullets_enemy;
-
+	FIND_CVARS(EXTERN_FIND_WRAPPER)
+	DEFINE_CVARS(EXTERN_DEFINE_WRAPPER)
+	
 	extern const std::vector<CVarWrapper*> allCVars;
 }

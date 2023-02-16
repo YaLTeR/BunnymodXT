58bb4ef229a6620bf960c0240030d140a45310de
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2023-02-16 08:48:05 +0400

    Fix comment in triangleapi.h

--- HLSDK/common/triangleapi.h
+++ HLSDK/common/triangleapi.h
@@ -50,9 +50,9 @@ typedef struct triangleapi_s
 	void		( *Brightness ) ( float brightness );
 	void		( *CullFace ) ( TRICULLSTYLE style );
 	int			( *SpriteTexture ) ( struct model_s *pSpriteModel, int frame );
-	int			( *WorldToScreen ) ( float *world, float *screen );  // Returns 1 if it's z clipped
+	int			( *WorldToScreen ) ( float *world, float *screen );  // Returns 1 if it's z clipped, last function in 1712 build
 	void		( *Fog ) ( float flFogColor[3], float flStart, float flEnd, int bOn ); // Works just like GL_FOG, flFogColor is r/g/b.
-	void		( *ScreenToWorld ) ( float *screen, float *world  ); // Smiley: Last function in 1712 build
+	void		( *ScreenToWorld ) ( float *screen, float *world  );
 	void		( *GetMatrix ) ( const int pname, float *matrix );
 	int			( *BoxInPVS ) ( float *mins, float *maxs );
 	void		( *LightAtPoint ) ( float *pos, float *value );

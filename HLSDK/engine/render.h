be57fa576bd9f50ae8aff0f60154e9c344270651
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2024-03-01 21:41:55 +0400

    Reverse-engineered 'refdef_t' structure
    In older versions of HLSDK there is no such function as V_CalcRefDef on client-side, therefore, if you need to get info about pparams->vieworg and pparams->viewangles, then you need to find a global variable called 'r_refdef' in the engine and then access from it by using this structure

---
+++ HLSDK/engine/render.h
@@ -0,0 +1,43 @@
+#if !defined( RENDERH )
+#define RENDERH
+#ifdef _WIN32
+#pragma once
+#endif
+
+typedef struct vrect_s
+{
+	int				x,y,width,height;
+	struct vrect_s	*pnext;
+} vrect_t;
+
+typedef struct
+{
+	vrect_t		vrect;				// subwindow in video for refresh
+									// FIXME: not need vrect next field here?
+	vrect_t		aliasvrect;			// scaled Alias version
+	int			vrectright, vrectbottom;	// right & bottom screen coords
+	int			aliasvrectright, aliasvrectbottom;	// scaled Alias versions
+	float		vrectrightedge;			// rightmost right edge we care about,
+										//  for use in edge list
+	float		fvrectx, fvrecty;		// for floating-point compares
+	float		fvrectx_adj, fvrecty_adj; // left and top edges, for clamping
+	int			vrect_x_adj_shift20;	// (vrect.x + 0.5 - epsilon) << 20
+	int			vrectright_adj_shift20;	// (vrectright + 0.5 - epsilon) << 20
+	float		fvrectright_adj, fvrectbottom_adj;
+										// right and bottom edges, for clamping
+	float		fvrectright;			// rightmost edge, for Alias clamping
+	float		fvrectbottom;			// bottommost edge, for Alias clamping
+	float		horizontalFieldOfView;	// at Z = 1.0, this many X is visible 
+										// 2.0 = 90 degrees
+	float		xOrigin;			// should probably allways be 0.5
+	float		yOrigin;			// between be around 0.3 to 0.5
+
+	vec3_t		vieworg;
+	vec3_t		viewangles;
+
+	color24		ambientlight;
+
+	qboolean	onlyClientDraws;
+} refdef_t;
+
+#endif

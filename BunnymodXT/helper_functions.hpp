d61d32498b3640901001514bceaaa3b87625dc2c
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2024-03-16 13:20:32 +0400

    helper_functions: implemented a custom function to print movetype name

--- BunnymodXT/helper_functions.hpp
+++ BunnymodXT/helper_functions.hpp
@@ -16,6 +16,7 @@ namespace helper_functions
 	float adjust_fov_for_widescreen(float fov, float def_aspect_ratio, float our_aspect_ratio);
 	bool is_entity_give_infinite_health(const edict_t* ent)
 	std::string get_flags(int flags);
+	std::string get_movetype(int movetype);
 
 	// https://github.com/ValveSoftware/halflife/blob/c7240b965743a53a29491dd49320c88eecf6257b/dlls/cbase.cpp#L706
 	int IsInWorld(Vector origin, Vector velocity, int map_size, int map_max_velocity);

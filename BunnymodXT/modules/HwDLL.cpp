a4ba1dfb338575d74c0a312d683346e659732f43
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2024-03-16 11:49:21 +0400

    Adjust FOV code moved to helper_functions module

--- BunnymodXT/modules/HwDLL.cpp
+++ BunnymodXT/modules/HwDLL.cpp
@@ -8165,10 +8165,10 @@ HOOK_DEF_0(HwDLL, void, __cdecl, R_SetFrustum)
 
 		if (fov != prev_calculated_fov)
 		{
-			float calculated_fov = static_cast<float>(std::atan(std::tan(fov * M_PI / 360.0f) * def_aspect_ratio * our_aspect_ratio) * 360.0f / M_PI);
+			float calculated_fov = helper_functions::adjust_fov_for_widescreen(fov, def_aspect_ratio, our_aspect_ratio);
 			*scr_fov_value = std::clamp(calculated_fov, 10.0f, 150.0f);
 
-			// Engine does the clamp of FOV if less 10 or higher than 150
+			// Engine does the clamp of FOV if less 10 or higher than 150 in SCR_CalcRefdef function
 			// Although, it could be extended to 1 for min. value and 179 for max. value
 
 			prev_calculated_fov = calculated_fov;

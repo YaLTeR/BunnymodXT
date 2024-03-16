2025ea9ceeb491a6bcb72a3624c2a54673ddfda2
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2024-03-16 12:21:12 +0400

    Implemented: IsBSPModel, ReflectGauss, Center, EyePosition, Intersects functions from HLSDK for helper_functions module

--- BunnymodXT/helper_functions.hpp
+++ BunnymodXT/helper_functions.hpp
@@ -10,8 +10,24 @@
 
 namespace helper_functions
 {
-	void com_fixslashes(std::string &str);
+	void com_fixslashes(std::string &str); // https://github.com/ValveSoftware/halflife/blob/c7240b965743a53a29491dd49320c88eecf6257b/game_shared/bot/nav_file.cpp#L680
 	std::string swap_lib(const char* current_lib_path, std::string new_lib_path, const char *start);
 	void crash_if_failed(std::string str);
 	float adjust_fov_for_widescreen(float fov, float def_aspect_ratio, float our_aspect_ratio);
+
+	// https://github.com/ValveSoftware/halflife/blob/c7240b965743a53a29491dd49320c88eecf6257b/dlls/cbase.h#L197-L198
+	bool IsBSPModel(int solid, int movetype);
+	bool IsBSPModel(const edict_t *ent);
+	bool ReflectGauss(int solid, int movetype, float takedamage);
+	bool ReflectGauss(const edict_t *ent);
+
+	// https://github.com/ValveSoftware/halflife/blob/c7240b965743a53a29491dd49320c88eecf6257b/dlls/cbase.h#L336-L337
+	Vector Center(Vector absmin, Vector absmax);
+	Vector Center(const edict_t *ent);
+	Vector EyePosition(Vector origin, Vector view_ofs);
+	Vector EyePosition(const edict_t *ent);
+
+	// https://github.com/ValveSoftware/halflife/blob/c7240b965743a53a29491dd49320c88eecf6257b/dlls/cbase.cpp#L673
+	bool Intersects(Vector absmin1, Vector absmax1, Vector absmin2, Vector absmax2);
+	bool Intersects(const edict_t *ent1, const edict_t *ent2);
 }

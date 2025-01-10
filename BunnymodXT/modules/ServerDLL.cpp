0dff51fc8c04823919ed8a71b5cd0bf5b7f00cd3
Pinsplash <39359267+Pinsplash@users.noreply.github.com>
2025-01-09 20:34:50 -0600

    fixed selfgauss debug and other trace problem
    bxt_hud_selfgauss
    * 2 and below show singleplayer behavior
    * 3 and above show multiplayer behavior
    * Even numbers show coordinates of line traces
    * All reasons for failing to selfgauss are now shown
    * Threshold translated to time value, more intuitive
    * Hit group display may have been broken before, definitely works now
    
    to fix bxt_hud_selfgauss not working correctly, one condition was removed from ServerDLL::TraceLine. this needs to be removed or else the 2nd and 3rd line traces incorrectly say they hit an object at 0, 0, 0.
    
    since the condition was added to avoid a crash when playing a demo in bxt_hud_entity_info, all places that used a trace involving the player will now exit harmlessly if the player entity doesn't exist

--- BunnymodXT/modules/ServerDLL.cpp
+++ BunnymodXT/modules/ServerDLL.cpp
@@ -3002,7 +3002,7 @@ TraceResult ServerDLL::TraceLine(const float v1[3], const float v2[3], int fNoMo
 {
 	TraceResult tr{};
 
-	if (pEngfuncs && pentToSkip)
+	if (pEngfuncs)
 		pEngfuncs->pfnTraceLine(v1, v2, fNoMonsters, pentToSkip, &tr);
 
 	return tr;
